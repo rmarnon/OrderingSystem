5c621552d9572877463475b54d0e87ab737f34ea
Rodrigo Marnon <rmarnon@yahoo.com.br>
2024-03-20 21:13:58 -0300

    Adjust program, settings and produc entity

--- Ordering.System.Backend/Ordering.System.Api/Program.cs
+++ Ordering.System.Backend/Ordering.System.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Ordering System",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -14,6 +25,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(s =>
+    {
+        s.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering System - API");
+        s.DocExpansion(DocExpansion.None);
+    });
 }
 
 app.UseHttpsRedirection();
--- Ordering.System.Backend/Ordering.System.Models/Models/Product.cs
+++ Ordering.System.Backend/Ordering.System.Models/Models/Product.cs
@@ -6,6 +6,6 @@
         public double Value { get; set; }
         public string Description { get; set; }
         public DateTime RegistrationDate { get; set; }
-        public List<OrderItem> Products { get; set; } = new();
+        public List<OrderItem> Items { get; set; } = new();
     }
 }
