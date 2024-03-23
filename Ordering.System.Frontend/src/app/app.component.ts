0c480f4e7348b64dd8b706452b8b15c0e3f9834f
Rodrigo Marnon <rmarnon@yahoo.com.br>
2024-03-23 11:30:55 -0300

    header base and logo

--- Ordering.System.Frontend/src/app/app.component.ts
+++ Ordering.System.Frontend/src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'Ordering.System.Frontend';
 }
---
+++ Ordering.System.Frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
